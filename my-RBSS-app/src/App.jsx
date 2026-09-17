d4efc61fe7a54e39b29d35dc647129c55dfd0780
neomasebe9 <neomasebe9@gmail.com>
2026-09-17 02:20:35 +0200

    added logo and card.jsx

--- my-RBSS-app/src/App.jsx
+++ my-RBSS-app/src/App.jsx
@@ -2,14 +2,16 @@
 import "./App.css";
 import {LikeButton} from './frontend/components/TestButton'
 import Button from './frontend/components/Button'
+import Logo from './frontend/components/Logo'
 
 
 function App() {
 
   return (
       <div>
+        <Logo/>
         <Button variant="solid" fullWidth>Sign in</Button>
-        <Button variant="outline">Cancel</Button>
+        <Button variant="outline" fullWidth>Cancel</Button>
       </div>
       
   );
